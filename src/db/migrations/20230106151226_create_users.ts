b558118e9691e85ad792d1fc0afbb61f5ce69e97
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-06 17:31:08 +0100

    create user migration

---
+++ src/db/migrations/20230106151226_create_users.ts
@@ -0,0 +1,16 @@
+import { Knex } from "knex";
+
+exports.up = async function (knex: Knex): Promise<any> {
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary();
+    table.string('name').notNullable();
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    table.string('role').notNullable().defaultTo('user');
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = async function (knex: Knex): Promise<any> {
+  return knex.schema.dropTableIfExists('users');
+};

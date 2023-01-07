7b62b0148dc092060569a36d8bc362bd2aa70083
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 03:19:26 +0100

    add login route in controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import {
+  Body,
+  Controller,
+  Post,
+} from '@nestjs/common/decorators';
+
+import { AuthService } from './auth.service';
+import { LoginDTO } from './auto.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('auth/login')
+  async login(@Body() body: LoginDTO) {
+    return this.authService.login(body);
+  }
+}

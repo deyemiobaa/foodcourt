32a591ffca1f79ddad47cb9a1067d7d86140637d
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 11:40:24 +0100

    add migrations for addons and addon-categories

---
+++ src/db/migrations/20230107102911_create_addons.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('addons', (table) => {
+    table.uuid('id').primary();
+    table.string('name', 100).notNullable();
+    table.integer('price').notNullable();
+    table.string('description', 500);
+    table.string('category');
+    table.uuid('brand_id')
+      .notNullable()
+      .references('id')
+      .inTable('brands')
+      .onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('addons');
+}
+
---
+++ src/db/migrations/20230107103724_create_addon_categories.ts
@@ -0,0 +1,21 @@
+import { Knex } from "knex";
+
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('addon_categories', (table) => {
+    table.uuid('id').primary();
+    table.string('name', 100).notNullable();
+    table.uuid('brand_id')
+      .notNullable()
+      .references('id')
+      .inTable('brands')
+      .onDelete('CASCADE');
+    table.timestamps(true, true);
+  });
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('addon_categories');
+}
+

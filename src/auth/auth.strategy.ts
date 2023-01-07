6a88d909e5c36e6ca924f2231467a4a0f4d56817
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 03:18:43 +0100

    define auth strategy

---
+++ src/auth/auth.strategy.ts
@@ -0,0 +1,28 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Inject, Injectable } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UnauthorizedException } from '@nestjs/common/exceptions';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    @Inject(ConfigService) private configService: ConfigService,
+    private readonly authService: AuthService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('jwt.secret')
+    });
+  }
+
+  async validate(email: string): Promise<any> {
+    const user = await this.authService.validateUser(email);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}

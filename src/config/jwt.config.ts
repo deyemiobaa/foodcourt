6b7b75cf6a4381bce084aa139511676dae5aed97
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 03:18:12 +0100

    jwt config

---
+++ src/config/jwt.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('jwt', () => ({
+  options: {
+    expiresIn: '30d',
+    algorithm: 'HS512'
+  },
+  secret: process.env.JWT_SECRET
+}));

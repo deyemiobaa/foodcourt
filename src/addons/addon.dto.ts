cfc67be9f778d7fcffe3ee03c97583046bbf7ce3
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 15:39:35 +0100

    create addon dto

---
+++ src/addons/addon.dto.ts
@@ -0,0 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, IsNotEmpty, IsNumber } from 'class-validator';
+
+export class AddonDTO {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  @IsOptional()
+  price?: number;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  category?: string;
+}
+
+export class PatchAddonDTO {
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @ApiProperty({ required: false })
+  @IsNumber()
+  @IsOptional()
+  price?: number;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  category?: string;
+}

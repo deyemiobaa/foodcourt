722f81710aadd2c549e7d822fd7a786dab71e3cd
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 13:03:41 +0100

    generate brands module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AddonsModule } from './addons/addons.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { BrandsController } from './brands/brands.controller';
+import { BrandsModule } from './brands/brands.module';
 import jwtConfig from './config/jwt.config';
 
 @Module({
@@ -13,11 +14,11 @@ import jwtConfig from './config/jwt.config';
       isGlobal: true,
       load: [jwtConfig]
     }),
-    AddonsModule,
     AuthModule,
-    UsersModule
+    UsersModule,
+    BrandsModule
   ],
-  controllers: [AppController],
+  controllers: [AppController, BrandsController],
   providers: [AppService],
 })
 export class AppModule {}

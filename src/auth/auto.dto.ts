39a01a91ade7759bcf2eb8368691d3cf14226994
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 03:22:11 +0100

    create data transfer object for login

---
+++ src/auth/auto.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsEmail,
+  MinLength,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class LoginDTO {
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @ApiProperty()
+  @IsString()
+  @MinLength(5)
+  password: string;
+}

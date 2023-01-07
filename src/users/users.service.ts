d8130a63a1137db48a9ff83373dad1c918363baf
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 19:37:03 +0100

    user service method to check for role and permission

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException, NotFoundException } from '@nestjs/common';
 import { LoginDTO } from 'src/auth/auth.dto';
 import User from 'src/db/models/user';
 
@@ -10,5 +10,25 @@ export class UsersService {
 
   async findOne(user: LoginDTO) {
     return User.query().findOne({ email: user.email, password: user.password });
+  }
+
+  async checkUserAndBrand(id: string, brandId: string): Promise<boolean> {
+    const user = await User.query()
+      .findById(id)
+      .withGraphFetched('brands');
+
+    if (!user) {
+      throw new NotFoundException();
+    }
+
+    if (user.role !== 'admin') {
+      throw new UnauthorizedException();
+    }
+
+    if (!user.brand) {
+      throw new NotFoundException();
+    }
+
+    return user.brand.id === brandId;
   }
 }

d2329cf0b1211c21ac934e1d64fc3ddc6de15aff
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 15:40:26 +0100

    services to valid brand before performing crud actions

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,4 +1,67 @@
 import { Injectable } from '@nestjs/common';
+import { AddonsService } from 'src/addons/addons.service';
+import Brand from 'src/db/models/brand';
+import { NotFoundException, ForbiddenException } from '@nestjs/common/exceptions'
+import { AddonDTO, PatchAddonDTO } from 'src/addons/addon.dto';
 
 @Injectable()
-export class BrandsService {}
+export class BrandsService {
+  constructor(private addonsService: AddonsService) { }
+
+  async getAddonsForBrand(brandId: string) {
+    const brand = await Brand.query().findById(brandId);
+    if (!brand) {
+      throw new NotFoundException()
+    }
+    const addon = await this.addonsService.getAddons(brandId);
+    return addon;
+  }
+
+  async getAddonForBrand(brandId: string, addonId: string) {
+    const brand = await Brand.query().findById(brandId);
+    if (!brand) {
+      throw new NotFoundException()
+    }
+    const addon = await this.addonsService.getAddon(addonId, brandId);
+    if (!addon) {
+      throw new NotFoundException()
+    }
+    return addon;
+  }
+
+  async createAddonForBrand(userId: string, brandId: string, body: AddonDTO) { 
+    const brand = await Brand.query().findById(brandId);
+    if (!brand) {
+      throw new NotFoundException()
+    }
+    const addon = await this.addonsService.createAddon(userId, brandId, body);
+    if (!addon) {
+      throw new ForbiddenException()
+    }
+    return addon;
+  }
+
+  async updateAddonForBrand(brandId: string, addonId: string, body: PatchAddonDTO) {
+    const brand = await Brand.query().findById(brandId);
+    if (!brand) {
+      throw new NotFoundException()
+    }
+    const addon = await this.addonsService.updateAddon(addonId, brandId, body);
+    if (!addon) {
+      throw new ForbiddenException()
+    }
+    return addon;
+  }
+
+  async deleteAddonForBrand(brandId: string, addonId: string) {
+    const brand = await Brand.query().findById(brandId)
+    if (!brand) {
+      throw new NotFoundException()
+    }
+    const addon = await this.addonsService.deleteAddon(addonId, brandId);
+    if (!addon) {
+      throw new ForbiddenException()
+    }
+    return addon;
+  }
+}

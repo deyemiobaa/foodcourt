b137675fe90678842d971664a59d8ec934af74e0
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2023-01-07 15:39:56 +0100

    services to perform CRUD actions on addons

--- src/addons/addons.service.ts
+++ src/addons/addons.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import Addon from 'src/db/models/addon';
+import { AddonDTO, PatchAddonDTO } from './addon.dto';
 
 @Injectable()
-export class AddonsService {}
+export class AddonsService {
+  async createAddon(userId: string, brandId: string, body: AddonDTO) {
+    return Addon.query().insert(
+      {
+        name: body.name,
+        description: body.description,
+        price: body.price,
+        category: body.category,
+        brandId: brandId,
+        userId: userId,
+      }
+    )
+  }
+
+  async getAddons(brandId: string) {
+    return Addon.query().where('brandId', brandId);
+  }
+
+  async getAddon(id: string, brandId: string) {
+    return Addon.query().findOne({ id, brandId });
+  }
+
+  async updateAddon(id: string, brandId: string, body: PatchAddonDTO) {
+    return Addon.query().patch({ ...body }).findOne({ id, brandId })
+  }
+
+  async deleteAddon(id: string, brandId: string) {
+    return Addon.query().delete().findOne({ id, brandId });
+  }
+}
